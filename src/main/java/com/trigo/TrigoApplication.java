d701295828cd78fe0ec3a97ce1b0507550f452ea
mangarmar <mangarmar17@alum.us.es>
2022-02-17 16:18:41 +0100

    Inicialización del proyecto

---
+++ src/main/java/com/trigo/TrigoApplication.java
@@ -0,0 +1,13 @@
+package com.trigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrigoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrigoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trigo/TrigoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
