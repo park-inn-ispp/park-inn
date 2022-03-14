45027824e84e9633a4e1adbf555c1b754f95ed20
xNegis <xnegis@gmail.com>
2022-03-14 18:03:37 +0100

    fixed cors

--- src/main/java/com/parkinn/configuration/SecurityConfiguration.java
+++ src/main/java/com/parkinn/configuration/SecurityConfiguration.java
@@ -85,7 +85,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Bean
 	CorsConfigurationSource corsConfigurationSource() {
     final CorsConfiguration configuration = new CorsConfiguration();
-    configuration.setAllowedOrigins(Arrays.asList("*"));
+    configuration.setAllowedOrigins(Arrays.asList("https://park-inn-ispp-fe.herokuapp.com/"));
     configuration.setAllowedMethods(Arrays.asList("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
     configuration.setAllowCredentials(true);
     configuration.setAllowedHeaders(Arrays.asList("*"));
