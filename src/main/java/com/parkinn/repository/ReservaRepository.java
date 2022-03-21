892d1eddb8925523c87c44bb0402ac2b75edcd33
manrivlla <manrivlla@alum.us.es>
2022-03-21 12:36:52 +0100

    Commit auxiliar

--- src/main/java/com/parkinn/repository/ReservaRepository.java
+++ src/main/java/com/parkinn/repository/ReservaRepository.java
@@ -1,5 +1,6 @@
 package com.parkinn.repository;
 
+import org.springframework.dao.DataAccessException;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDateTime;
@@ -9,11 +10,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-
+import com.parkinn.model.Plaza;
 import com.parkinn.model.Reserva;
 
 public interface ReservaRepository extends JpaRepository<Reserva, Long> {
 
+	 @Query(value = "SELECT DISTINCT r.* FROM Reservas r WHERE r.plaza_id LIKE :plaza_id", nativeQuery=true)
+	 public List<Reserva> findByPlazaId(@Param("plaza_id") Long id) throws DataAccessException;
+	   	
+	
+	
+	
 }
 	
 
