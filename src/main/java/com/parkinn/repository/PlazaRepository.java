6fb60c41ec36380ed0e7d31fb47f1e7180a8070c
manrivlla <manrivlla@alum.us.es>
2022-03-21 12:12:36 +0100

    Devolver una lista de plazas dado el id de un usuario
    #13 Seleccionar y mostrar todas las plazas que un usuario tiene a su
    disposición para que cuando esté en su cuenta pueda verlas

--- src/main/java/com/parkinn/repository/PlazaRepository.java
+++ src/main/java/com/parkinn/repository/PlazaRepository.java
@@ -19,6 +19,9 @@ public interface PlazaRepository extends JpaRepository<Plaza, Long> {
 
     @Query(value = "SELECT p FROM Park p WHERE p.direccion LIKE :direccion", nativeQuery=true)
    	public Plaza findByAddress(@Param("direccion")String direccion) throws DataAccessException;
+    
+    @Query(value = "SELECT DISTINCT p.* FROM Plazas p WHERE p.user_id LIKE :usuario_id", nativeQuery=true)
+    public List<Plaza> findByUserId(@Param("usuario_id") Long id) throws DataAccessException;
    	
 }
 
