06444ab2bbacbb794f750f39486cfd5c6152e732
Jose Manuel <Jose Manuel@DESKTOP-3K3KKC9.mshome.net>
2022-03-21 12:14:10 +0100

    Devolver lista de reservas por usuario
    #11 Al buscar las reservas de un usario en concreto se devuelve la lista
    de reservas asignadas a ese usuario

--- src/main/java/com/parkinn/repository/ReservaRepository.java
+++ src/main/java/com/parkinn/repository/ReservaRepository.java
@@ -3,12 +3,16 @@ package com.parkinn.repository;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import org.springframework.dao.DataAccessException;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import com.parkinn.model.Plaza;
 import com.parkinn.model.Reserva;
 
 public interface ReservaRepository extends JpaRepository<Reserva, Long> {
 	
-}
+	 @Query(value = "SELECT DISTINCT r.* FROM Reservas r WHERE r.user_id LIKE :usuario_id", nativeQuery=true)
+	   	public List<Reserva> findByUserId(@Param("usuario_id")Long id) throws DataAccessException;
+} 
---
+++ src/main/java/com/parkinn/web/ReservaController.java
@@ -0,0 +1,44 @@
+package com.parkinn.web;
+
+	import java.net.URI;
+	import java.net.URISyntaxException;
+	import java.time.LocalDateTime;
+	import java.util.HashMap;
+	import java.util.List;
+	import java.util.Map;
+
+	import javax.validation.Valid;
+
+	import com.parkinn.model.Plaza;
+	import com.parkinn.model.Reserva;
+	import com.parkinn.service.PlazaService;
+	import com.parkinn.service.ReservaService;
+
+
+	import org.springframework.web.bind.annotation.DeleteMapping;
+	import org.springframework.web.bind.annotation.GetMapping;
+	import org.springframework.web.bind.annotation.PathVariable;
+	import org.springframework.web.bind.annotation.PostMapping;
+	import org.springframework.web.bind.annotation.PutMapping;
+	import org.springframework.web.bind.annotation.RequestBody;
+	import org.springframework.web.bind.annotation.RequestMapping;
+	import org.springframework.web.bind.annotation.RequestParam;
+	import org.springframework.web.bind.annotation.RestController;
+	import org.springframework.beans.factory.annotation.Autowired;
+	import org.springframework.format.annotation.DateTimeFormat;
+	import org.springframework.http.ResponseEntity;
+
+	@RestController
+	@RequestMapping("/reservas")
+	public class ReservaController {
+
+	
+	    @Autowired
+	    private ReservaService reservaService;
+
+	    @GetMapping("/usuario/{id}")
+	    public List<Reserva> reservasUsuario(@PathVariable Long id){
+	    	return reservaService.findByUserId(id);
+	    }
+	
+	}
