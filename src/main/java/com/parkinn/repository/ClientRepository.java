5eb633573483b61532d0a50632453c51af1cb4db
Sergio <serrojjim@alum.us.es>
2022-03-23 19:56:49 +0100

    #47 modificado logout y nuevo endpoint bymail

--- src/main/java/com/parkinn/repository/ClientRepository.java
+++ src/main/java/com/parkinn/repository/ClientRepository.java
@@ -1,8 +1,15 @@
 package com.parkinn.repository;
 
+import org.springframework.dao.DataAccessException;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 import com.parkinn.model.Client;
 
 public interface ClientRepository extends JpaRepository<Client, Long> {
+
+    public Optional<Client> findByEmail(@Param("email")String email) throws DataAccessException;
+
 }
