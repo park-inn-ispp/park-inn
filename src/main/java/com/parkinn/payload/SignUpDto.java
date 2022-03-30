11373af2d80db04448acb4ddb9704807e019c06f
Miguel Angel <miguenieva2000@gmail.com>
2022-03-30 19:34:02 +0200

    feature/26 - Spring Sign Up Done

--- src/main/java/com/parkinn/payload/SignUpDto.java
+++ src/main/java/com/parkinn/payload/SignUpDto.java
@@ -5,6 +5,11 @@ public class SignUpDto {
     private String name;
     private String email;
     private String password;
+    private String confirm;
+    private String phone;
+    private String surname;
+
+
     public String getName() {
         return name;
     }
@@ -23,6 +28,24 @@ public class SignUpDto {
     }
     public void setPassword(String password) {
         this.password = password;
+    }
+    public String getConfirm() {
+        return confirm;
+    }
+    public void setConfirm(String confirm) {
+        this.confirm = confirm;
+    }
+    public String getPhone() {
+        return phone;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    public String getSurname() {
+        return surname;
+    }
+    public void setSurname(String surname) {
+        this.surname = surname;
     }
 
     
