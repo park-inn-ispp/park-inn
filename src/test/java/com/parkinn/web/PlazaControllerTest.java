32b501dfae177294d9edca54bae80a4c16e59883
Sergio <serrojjim@alum.us.es>
2022-04-26 18:20:09 +0200

    Arreglado test controller

--- src/test/java/com/parkinn/web/PlazaControllerTest.java
+++ src/test/java/com/parkinn/web/PlazaControllerTest.java
@@ -2,13 +2,17 @@ package com.parkinn.web;
 
 import com.parkinn.configuration.SecurityConfiguration;
 import com.parkinn.model.Client;
+import com.parkinn.model.Plaza;
 import com.parkinn.repository.HorarioRepository;
 import com.parkinn.repository.PlazaRepository;
 import com.parkinn.repository.ReservaRepository;
 import com.parkinn.repository.RoleRepository;
 import com.parkinn.service.ClientService;
 import com.parkinn.service.CustomUserDetailsService;
+import com.parkinn.service.HorarioService;
 import com.parkinn.service.MailService;
+import com.parkinn.service.PlazaService;
+import com.parkinn.service.ReservaService;
 
 import static org.mockito.BDDMockito.given;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
@@ -22,61 +26,75 @@ import javax.sql.DataSource;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.context.annotation.FilterType;
 
-@WebMvcTest(controllers = ClientsController.class)
-public class ClientsControllerTest {
+@WebMvcTest(controllers = PlazaController.class)
+public class PlazaControllerTest {
 
-	@MockBean
-	ClientService service;
-
-	@MockBean
-    org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder passwordEncoder;
-    
-	@MockBean
-    PlazaRepository plazaRepository;
-    
-	@MockBean
-    RoleRepository roleRepository;
-    
-	@MockBean
-    HorarioRepository horarioRepository;
-    
-	@MockBean
-    ReservaRepository reservasRepository;
-
-	@MockBean
-	MailService mailService;
+	@Autowired
+	MockMvc mockMvc;
 
 	@MockBean
 	CustomUserDetailsService customUserDetailsService;
 
 	@MockBean
+	PlazaService plazaService;
+
+	@MockBean
+	ReservaService reservaService;
+
+	@MockBean
+	HorarioService horarioService;
+
+	@MockBean
+    private HorarioRepository horarioRepository;
+    
+	@MockBean
+    private ReservaRepository reservasRepository;
+
+	@MockBean
+	MailService mailService;
+
+	@MockBean
 	DataSource dataSource;
+
+
+	// @MockBean
+    // private AuthenticationManagerResolver auth;
 	
-	@Autowired
-	MockMvc mockMvc;
+
+	/* @MockBean
+    private CustomUserDetailsService UserDetailsService;
+
+	@MockBean
+	DataSource dataSource;
+ */
 
 	@BeforeEach
 	void setup() {
 		Client c1 = new Client(1l);
-		Client c2 = new Client(2l);
-		given(this.service.findAll()).willReturn(asList(c1,c2));
+		Plaza p1 = new Plaza(1l,c1);
+		Plaza p2 = new Plaza(2l,c1);
+		given(this.plazaService.findAll()).willReturn(asList(p1,p2));
 	}
 
 	@WithMockUser(value = "spring")
     @Test
-	void testGetClients() throws Exception {
-		mockMvc.perform(get("/clients")).andExpect(status().isOk())
+	void testGetPlazas() throws Exception {
+		mockMvc.perform(get("/plazas/all")).andExpect(status().isOk())
 			.andExpect(jsonPath("$.size()", Matchers.is(2)));
 	}
+
 }
